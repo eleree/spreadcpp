a50eb9ff24773c07184e760b53cb01733855fbc8
Chin <1581521797@qq.com>
2016-07-05 11:17:47 +0800

    Add Test Code

---
+++ SpreadCpp/include/cpp/util/Base64.h
@@ -0,0 +1,44 @@
+#ifndef _ANYMESH_BASE64_H_
+#define _ANYMESH_BASE64_H_
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdint.h>
+#include <cstdio>
+
+const char Base64::_base64EncodeTable[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+const char Base64::_base64DecodeTable[256] =
+{
+	/* ASCII table */
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 62, 64, 64, 64, 63,
+	52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 64, 64, 64, 64, 64, 64,
+	64, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+	15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 64, 64, 64, 64, 64,
+	64, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+	41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64
+};
+
+using namespace std;
+class Base64{
+public:
+	Base64();
+	~Base64();
+	int32_t encode(const char * inputData, const uint32_t inputLen, char * outputBuf, uint32_t * outputLen, uint32_t maxOutputLen);
+	int32_t decode(const char * inputData, const uint32_t inputLen, char * outputBuf, uint32_t * outputLen, uint32_t maxOutputLen);
+private:
+	static const char _base64EncodeTable[65];
+	static const char _base64DecodeTable[256];
+};
+
+#endif
