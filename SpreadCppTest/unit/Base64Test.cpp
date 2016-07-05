47c96b507e815a03a3d4a7c0237b4f15e4cfc043
Chin <1581521797@qq.com>
2016-07-05 14:18:35 +0800

    Update Project Configurations

---
+++ SpreadCppTest/unit/Base64Test.cpp
@@ -0,0 +1,13 @@
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+
+
+int main(int argc, char ** argv)
+{
+	return 0;
+}
