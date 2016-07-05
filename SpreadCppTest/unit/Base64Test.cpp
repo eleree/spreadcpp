7816e46f4b4eaba60844c20ca6e7504f45ca9aec
Chin <1581521797@qq.com>
2016-07-05 16:26:51 +0800

    Add CppUnit Framework

--- SpreadCppTest/unit/Base64Test.cpp
+++ SpreadCppTest/unit/Base64Test.cpp
@@ -5,9 +5,50 @@
 #include <cppunit/TestResult.h>
 #include <cppunit/TestResultCollector.h>
 #include <cppunit/TestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/config/SourcePrefix.h>
+
+#include <cpp/util/Base64.h>
+
+class Base64Test : public CPPUNIT_NS::TestFixture
+{
+	CPPUNIT_TEST_SUITE(Base64Test);
+	CPPUNIT_TEST(testSolution);
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	void testSolution();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(Base64Test);
+
+void Base64Test::testSolution()
+{
+
+	CPPUNIT_ASSERT_EQUAL(0, 0);
+	CPPUNIT_ASSERT_EQUAL(2, 2);
+	CPPUNIT_ASSERT_EQUAL(2, 2);
+	CPPUNIT_ASSERT_EQUAL(2, 2);
+
+}
 
 
 int main(int argc, char ** argv)
 {
+	std::cout << "Bas64 Test" << std::endl;
+	CPPUNIT_NS::TestResult controller;
+	CPPUNIT_NS::TestResultCollector result;
+	controller.addListener(&result);
+
+	CPPUNIT_NS::BriefTestProgressListener progress;
+	controller.addListener(&progress);
+
+	CPPUNIT_NS::TestRunner runner;
+	runner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
+	runner.run(controller);
+
+	CPPUNIT_NS::CompilerOutputter outputter(&result, CPPUNIT_NS::stdCOut());
+	outputter.write();
+
 	return 0;
 }
